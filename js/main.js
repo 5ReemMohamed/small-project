dddcf3d28c26d60c53ec674bc0cb111e4a3fdad7
reemmohamed8433@gmail.com <reemmohamed8433@gmail.com>
2025-07-30 23:38:29 +0300

    edit

--- js/main.js
+++ js/main.js
@@ -17,3 +17,15 @@
       prevEl: '.swiper-button-prev'
     }
   });
+
+   const scrollBtn = document.getElementById("scrollTopBtn");
+  window.onscroll = function () {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+      scrollBtn.style.display = "block";
+    } else {
+      scrollBtn.style.display = "none";
+    }
+  };
+  scrollBtn.onclick = function () {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
